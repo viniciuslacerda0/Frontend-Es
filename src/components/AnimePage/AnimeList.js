9ace6cc8abae8aebb552eeb149c4b1c828a7b5d2
José Vinícius Lacerda de Arruda <jose.arruda@ccc.ufcg.edu.br>
2018-11-19 21:29:36 -0200

    Refatorando para entrada da paginação

--- src/components/AnimePage/AnimeList.js
+++ src/components/AnimePage/AnimeList.js
@@ -3,16 +3,55 @@ import {Row, Col, Grid} from 'react-bootstrap';
 import '../Styles/styles.css'
 import Topo from '../Template/Topo'
 import ShowAnimes from '../AnimePage/ShowAnimes'
+import Propaganda from '../Template/Propaganda'
+import PageHeader from '../Template/PageHeader'
+import Pagination from '../Pagination/Pagination'
+
+export default class AnimeList extends Component {
+
+	constructor(props){
+		super(props);
+		var listAnimes = [{name: "nanatsu no taizai", thumb: "https://i.kym-cdn.com/photos/images/newsfeed/001/155/275/559.gif" },
+        {name: "naruto", thumb: "https://i.kym-cdn.com/photos/images/newsfeed/001/155/275/559.gif" },
+        {name: "dragon ball",  thumb: "https://i.kym-cdn.com/photos/images/newsfeed/001/155/275/559.gif" },
+        {name: "fulmmetal", thumb: "https://i.kym-cdn.com/photos/images/newsfeed/001/155/275/559.gif" },
+        {name: "fulmmetal", thumb: "https://i.kym-cdn.com/photos/images/newsfeed/001/155/275/559.gif" },
+        {name: "fulmmetal", thumb: "https://i.kym-cdn.com/photos/images/newsfeed/001/155/275/559.gif" },]
+
+        this.onChangePage = this.onChangePage.bind(this);
+
+         this.state = { list: [], pageOfItems: [], exampleItems: listAnimes }
+
+	}
+
+	 onChangePage(pageOfItems){
+        this.setState({ pageOfItems: pageOfItems});
+    }
 
 
-
-const ShowAnimes = () =>(
-        <div>
+	render() {
+		return(
+		<div>
 			<Topo/>
-            <ShowAnimes/>
-		</div>
-);
-  
+				<Propaganda/>
+				<div className='container todo'>
+				
+			<Grid >
+				<Col md={9}>
+					<Row>
+					    <ShowAnimes list={this.state.pageOfItems}/>
+                        <Pagination items={this.state.exampleItems} onChangePage={this.onChangePage}/>
+					</Row>
+				</Col>
+				<Col md={3}>
+					<PageHeader name='Top anime'/>
+				</Col>
+			</Grid>
+			</div>
+			</div>
+		)
+	}
 
 
-export default ShowAnimes;
+}
+
--- src/components/AnimePage/AnimePage.js
+++ src/components/AnimePage/AnimePage.js
@@ -1,18 +1,58 @@
 import React from 'react';
 import {Row, Col, Grid} from 'react-bootstrap';
 import '../Styles/styles.css'
+import Anime from '../AnimePage/Anime'
 import Topo from '../Template/Topo'
-import Anime from './Anime'
+import Propaganda from '../Template/Propaganda'
+import PageHeader from '../Template/PageHeader'
+import Pagination from '../Pagination/Pagination'
+
+export default class AnimePage extends Component {
+
+        constructor(props){
+		super(props);
+		var listAnimes = [{name: "nanatsu no taizai", episode: 1, thumb: "https://i.kym-cdn.com/photos/images/newsfeed/001/155/275/559.gif" },
+                {name: "naruto", episode: 2, thumb: "https://i.kym-cdn.com/photos/images/newsfeed/001/155/275/559.gif" },
+                {name: "dragon ball", episode: 3, thumb: "https://i.kym-cdn.com/photos/images/newsfeed/001/155/275/559.gif" },
+                {name: "fulmmetal", episode: 4, thumb: "https://i.kym-cdn.com/photos/images/newsfeed/001/155/275/559.gif" },
+                {name: "fulmmetal", episode: 5, thumb: "https://i.kym-cdn.com/photos/images/newsfeed/001/155/275/559.gif" },
+                {name: "fulmmetal", episode: 6, thumb: "https://i.kym-cdn.com/photos/images/newsfeed/001/155/275/559.gif" },]
+
+                this.onChangePage = this.onChangePage.bind(this);
+
+                this.state = { list: [], pageOfItems: [], exampleItems: listAnimes }
+
+	}
+
+	onChangePage(pageOfItems){
+                this.setState({ pageOfItems: pageOfItems});
+        }
 
 
+	render() {
+		return(
+		<div>
+			<Topo/>
+				<Propaganda/>
+				<div className='container todo'>
+				
+			<Grid >
+				<Col md={9}>
+					<Row>
+					        <Anime list={this.state.pageOfItems}/>
+                                                <Pagination items={this.state.exampleItems} onChangePage={this.onChangePage}/>
+					</Row>
+				</Col>
+				<Col md={3}>
+					<PageHeader name='Top anime'/>
+				</Col>
+			</Grid>
+			</div>
+			</div>
+		)
+	}
 
-const AnimePage = () =>(
-        <div>
-	        <Topo/>
-                <Anime/>
-	</div>
-);
-  
 
+}
 
 export default AnimePage;
