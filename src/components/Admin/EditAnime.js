62652c22d441789002908e51ca90090e641c7ec3
ramon <jose.ramon.silva@ccc.ufcg.edu.br>
2018-11-25 01:44:20 -0300

    iniciando edicao de animes

--- src/components/Admin/EditAnime.js
+++ src/components/Admin/EditAnime.js
@@ -6,60 +6,68 @@ import {Grid, FormControl, Button, Form, FormGroup} from 'react-bootstrap';
 import Axios from 'axios';
 
 class Animes extends React.Component {
-    constructor (props){
-        super(props);
-        this.state = {
-            data: [],
-            anime: ""
-      }
-      this.handleChange = this.handleChange.bind(this);
-      Axios.get('http://34.239.129.125/animes').then(response => response.data.content.animes).then(res => this.setState({data: res}));
+  constructor (props){
+      super(props);
+      this.state = {
+          data: [],
+          search: "",
+          change: false,
     }
+    this.search = this.search.bind(this);
+    this.handleChangeAnimeName = this.handleChangeAnimeName.bind(this);
+  }
 
-    handleChange(e) {
-      e.preventDefault();
-      this.setState({ anime: e.target.value });
 
-    }
+  handleChangeAnimeName(event){
+    this.setState({search: event.target.value});
 
-    render(){
-        return (
-            <div >
-              <Grid>
-                <PageHeader name="Filtrar um Anime:"/>
-                <div className="filter">
-                  <Form inline>
-                    <FormGroup>
-                    <FormControl
-                        style={{"marginRight":"10px"}}
-                        placeholder="Filtrar Anime"
-                        type="text"
-                        onChange={this.handleChange}/>
-                    </FormGroup>
-                    <Button type="submit" bsStyle="success">Filtrar</Button>
-                    <a href="/admin"><Button bsStyle="danger">Voltar</Button></a>
+  }
 
-                  </Form>
-                </div>
-                <PageHeader name="Todos os Animes Cadastrados:"/>
-                <table className="table">
-                    <tbody>
+  search() {
+    Axios.get("http://34.239.129.125/animes?search=" + this.state.search)
+    .then(response => this.setState({data:response.data.content.animes}))
+    .then(res => this.setState({change:true}))
+  }
 
-                        <tr>
-                          <td><p className="center"><strong>Nome</strong></p></td>
-                          <td><p className="center"><strong>Gênero</strong></p></td>
-                          <td><p className="center"><strong>Descrição</strong></p></td>
-                          <td><p className="center"><strong>Apagar</strong></p></td>
-                          <td><p className="center"><strong>Editar</strong></p></td>
-                        </tr>
-                        {this.state.data.map((anime, i) => <Anime key = {i} data = {anime} />)}
-                    </tbody>
-                </table>
-              </Grid>
-            </div>
 
-        );
-    }
+  render(){
+      return (
+          <div >
+            <Grid>
+              <PageHeader name="Filtrar um Anime:"/>
+              <div className="filter">
+                <Form inline>
+                  <FormGroup>
+                  <FormControl
+                      style={{"marginRight":"10px"}}
+                      placeholder="Filtrar Anime"
+                      type="text"
+                      onChange={this.handleChangeAnimeName}/>
+                  </FormGroup>
+                  <Button bsStyle="success" onClick={this.search}>Filtrar</Button>
+                  <a href="/admin"><Button bsStyle="danger">Voltar</Button></a>
+                </Form>
+
+              </div>
+              <PageHeader name="Todos os Animes Cadastrados:"/>
+              <table className="table">
+                  <tbody>
+
+                      <tr>
+                        <td><p className="center"><strong>Nome</strong></p></td>
+                        <td><p className="center"><strong>Gênero</strong></p></td>
+                        <td><p className="center"><strong>Descrição</strong></p></td>
+                        <td><p className="center"><strong>Apagar</strong></p></td>
+                        <td><p className="center"><strong>Editar</strong></p></td>
+                      </tr>
+                      {this.state.change ? this.state.data.map((anime) => <Anime data={anime} />) : null}
+                  </tbody>
+              </table>
+            </Grid>
+          </div>
+
+      );
+  }
   }
 
 export default Animes;
