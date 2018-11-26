64a2a574eef2f38b4c4b5d7ed99c2ab549ffda2f
Kleberson <kleberson.canuto@ccc.ufcg.edu.br>
2018-11-26 00:26:45 -0300

    pagina de generos e url condicional

--- src/components/GenresPage/GenresPage.js
+++ src/components/GenresPage/GenresPage.js
@@ -7,7 +7,7 @@ import PageHeader from '../Template/PageHeader';
 import Propaganda from '../Template/Propaganda';
 import Topo from '../Template/Topo';
 import Genre from './Genre';
-
+import Axios from 'axios'
 import './GenresPage.css';
 
 export default class GenresPage extends React.Component {
@@ -16,22 +16,10 @@ export default class GenresPage extends React.Component {
 		super(props);
 		this.state = {
 			genres : [
-				{nome: "açao", animes:['1', '2', '3', '4'], len: 4},
-				{nome: "aventura", animes: ['2', '3', '7', '10', '11', '14', '20', '21', '50'], len: 9},
-				{nome: "comedia", animes: ['2', '3', '7', '10', '14', '20', '21', '50'], len: 8},
-				{nome: "drama", animes: ['2', '3', '7', '10', '11', '14', '20', '21', '50'], len: 9},
-				{nome: "kawaii", animes: ['2', '3', '7', '10', '11', '14'], len: 6},
-				{nome: "shoujo", animes: ['2', '3', '7', '10', '11', '14', '20', '21', '50'], len: 9},
-				{nome: "shonen", animes: ['2', '3', '14', '20', '21', '50'], len: 6},
-				{nome: "ecchi", animes: ['2', '3', '7', '10', '11', '14', '20', '21'], len: 8},
-				{nome: "epic", animes: ['2', '3', '7', '10', '11', '14', '20'], len: 7},
-				{nome: "zombies", animes: ['2', '3', '7', '10', '11', '14', '20', '21', '50'], len: 9},
-				{nome: "ninjas", animes: ['2', '50'], len: 2},
-				{nome: "yuri", anime: ['2', '3', '7', '10', '11', '14', '20', '21', '50'], len: 9},
-				{nome: "sports", animes: ['2', '3', '7', '10', '11', '14', '50'], len: 7},
-				{nome: "terror", animes: ['2', '3', '7'], len: 3},
+				
 			]
 		}
+		Axios.get('http://34.239.129.125/animes/genres').then(res => this.setState({genres: res.data.content.genres})).catch(() => alert("error"));
 	}
 
 	render() {
@@ -46,13 +34,12 @@ export default class GenresPage extends React.Component {
 							<PageHeader name="Gêneros"/>
 						</Row>
 						<main>
-							{this.state.genres.map(a => 
-								<Link 	className="genreBox" 
-										to={{ 
-											pathname: `/generos/${a.nome}`,
-											state: { nome: `${a.nome.charAt(0).toUpperCase() + a.nome.slice(1)}`}
-								}}> 
-									{a.nome.toUpperCase()}({a.len}) 
+							{this.state.genres.map(a =>
+								<Link className="genreBox"
+										to={{
+											pathname: `/generos/${a}`,
+								}}>
+									{a}
 								</Link>
 							)}
 						</main>
@@ -68,4 +55,4 @@ export default class GenresPage extends React.Component {
 	}
 
 
-} 
+}
